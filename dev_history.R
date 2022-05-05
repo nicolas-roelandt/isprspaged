a573c0b167540a7582ff3afe232783c80a9b0b44
Nicolas Roelandt <roelandtn.pro@gmail.com>
2022-05-05 08:42:30 +0200

    Initial commit

---
+++ dev_history.R
@@ -0,0 +1,16 @@
+library(here)
+# usethis::create_project(path = here::here("InProgress/00_AME/isprs-paged") )
+
+usethis::use_git()
+
+usethis::use_description(
+  fields = list(
+    Title = "Pagedown template for ISPRS publication",
+    Description = paste("Pagedown template for ISPRS publication"),
+    `Authors@R` = c(
+      person("Nicolas", "Roelandt", email = "nicolas.roelandt@univ-eiffel.fr",
+             role = c("aut", "cre"), comment = c(ORCID = "0000-0001-9698-4275")),
+      person(given = "Uni. Gustave Eiffel", role = "cph")
+    )
+  ))
+
